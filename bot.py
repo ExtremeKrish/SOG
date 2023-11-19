e3861271b29ff76e6e49d903e23c44a2e5d51b46
Krish Vishwakarma <79414142+ExtremeKrish@users.noreply.github.com>
2023-11-19 19:10:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,96 +1,30 @@
-import re
-import random
+from telegram.ext import Updater, MessageHandler, CommandHandler
+from telegram import Update
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+TOKEN = '6171891363:AAEgSTM5I5w2ljZEl4uJ6cdgFmHJWqnHOmQ'
 
-import requests
-from lxml import html
-from telegram.ext import Updater, CommandHandler
+def start(update, context):
+    update.message.reply_text('Hello! I am your echo bot. Type anything, and I will repeat it.')
 
-DEBUG = 1
-
-def __debug(msg):
-    if DEBUG:
-        print("\033[94m[DEBUG]\033[0m", msg)
-
-def start(bot, update):
-    __debug("CALL start")
-
-    update.message.reply_text(
-        "Selam! Sana nasıl yardım edebileceğimi öğrenmek için /help yazabilirsin.")
-
-def help(bot, update):
-    __debug("CALL help")
-
-    update.message.reply_text(
-        "/video_at - sana Youtube’da şu an trend olan videolardan birini atarım.\n"
-		"/haber_at - sana BBC’de en çok okunan haberlerden birini atarım.\n\n"
-        "/help - bu yardım mesajını gösterir")
-
-def video_at(bot, update):
-    __debug("CALL video_at")
-
-    __debug(
-        "video_at: Sending request to https://www.youtube.com/feed/trending")
-
-    pg = requests.get("https://www.youtube.com/feed/trending")
-
-    __debug("video_at: Got response: %d" % pg.status_code)
-
-    if pg.status_code != 200:
-        update.message.reply_text("Aman! Bir hata oldu :(")
-        return
-
-    video_list = re.findall('href="/watch\?v=\w{11}"', pg.text)
-
-    if len(video_list) == 0:
-        update.message.reply_text("Aman! Bir hata oldu! Hiç video bulamadım :(")
-        return
-
-    update.message.reply_text("https://youtube.com" + random.choice(video_list)[6:-1])
-
-def haber_at(bot, update):
-    __debug("CALL haber_at")
-
-    __debug("haber_at: Sending request to https://www.bbc.com/news")
-
-    pg = requests.get("https://www.bbc.com/news")
-
-    __debug("haber_at: Got response: %d" % pg.status_code)
-
-    if pg.status_code != 200:
-        update.message.reply_text("Aman! Bir hata oldu :(")
-        return
-
-    tree = html.fromstring(pg.text)
-
-    news_list = list()
-    for i in range(1, 11):
-        elem = tree.xpath("//li[@data-entityid='most-popular-read-{}']//a".format(i))
-
-        if elem != []:
-            news_list.append(elem[0])
-
-    if len(news_list) == 0:
-        update.message.reply_text("Aman! Bir hata oldu! Hiç haber bulamadım :(")
-        return
-
-    update.message.reply_text("https://www.bbc.com" + random.choice(news_list).attrib["href"])
+def echo(update, context):
+    user_input = update.message.text
+    update.message.reply_text(f'You said: {user_input}')
 
 def main():
-    __debug("CALL main")
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-    __debug("main: Initializing updater")
-    updater = Updater("6171891363:AAEgSTM5I5w2ljZEl4uJ6cdgFmHJWqnHOmQ")
-    dp = updater.dispatcher
+    # Handlers
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(Filters.text & ~Filters.command, echo)
 
-    __debug("main: Configuring handlers")
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("video_at", video_at))
-    dp.add_handler(CommandHandler("haber_at", haber_at))
+    # Add handlers to the dispatcher
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(echo_handler)
 
-    __debug("main: Starting idle process")
+    # Start the Bot
     updater.start_polling()
     updater.idle()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
